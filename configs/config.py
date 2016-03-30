b374c8dd6fcccac70d50edb815707853a93638ef
Maria Cerase <maria.cerase@gmail.com>
2016-03-30 18:44:21 +0100

    better config file  name

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,9 @@
 __author__ = 'ciacicode'
 
+
 class Config(object):
     SECRET_KEY = 'production key'
     CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = 'sqlalchemyurl'
     UPLOAD_FOLDER = 'upload folder for receipts'
+
