9a35297b20eb6aefe62999ebf40491ce4a0bcc16
Maria Cerase <maria.cerase@gmail.com>
2016-03-31 13:39:33 +0000

    secret

--- configs/config.py
+++ configs/config.py
@@ -2,7 +2,7 @@ __author__ = 'ciacicode'
 
 
 class Config(object):
-    SECRET_KEY = 'very secret key'
+    SECRET_KEY = 'aa76&6(w1osj5y_!$cy0up&qtf3!b*%9%1dvmj8ujns5r&mfaw-1a9'
     CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@/egg?unix_socket=/cloudsql/egg-api:egg'
     UPLOAD_FOLDER = '/home/maria/Desktop/ciacicode/recipe_suggest/egg/receipts/'
