1bf32af93374acae1a7a93cc916abe773aa3ca0b
Maria Cerase <maria.cerase@gmail.com>
2016-03-31 14:53:49 +0100

    fucking config file

--- configs/config.py
+++ configs/config.py
@@ -2,8 +2,8 @@ __author__ = 'ciacicode'
 
 
 class Config(object):
-    SECRET_KEY = 'very secret key'
+    SECRET_KEY = '6AA0p[$f//LOLlpw'
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@/egg?unix_socket=/cloudsql/egg-api:egg'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://egg_admin:dev_admin@localhost:3306/egg'
     UPLOAD_FOLDER = '/home/maria/Desktop/ciacicode/recipe_suggest/egg/receipts/'
     ALLOWED_EXTENSIONS = 'pdf'
