c0615a98c2f3a8d3145eccc1aa43f503f5d95cd7
Maria Cerase <maria.cerase@gmail.com>
2016-04-07 19:26:55 +0100

    added cloud storage library

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -9,8 +9,7 @@ from google.appengine.api import mail
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
-        file_name = mail_message.attachments[0]
-        logging.info("Attachment file name: " + str(file_name))
+        file
 
 
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
